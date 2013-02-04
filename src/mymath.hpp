07216e90f7edcabe993b74746fd79c9164d488f1
jpupu <arthamster@gmail.com>
2013-02-04 22:41:48 +0200

    made math headers more portable

--- src/mymath.hpp
+++ src/mymath.hpp
@@ -1,6 +1,6 @@
 #ifndef MYMATH_HPP
 #define MYMATH_HPP
-
+#include <cmath>
 #include <glm/glm.hpp>
 
 using glm::vec2;
@@ -11,6 +11,9 @@ using glm::cross;
 using glm::normalize;
 using glm::clamp;
 
+#ifndef M_PI
+#define M_PI  3.14159265358979323846
+#endif
 #define M_2PI (2*M_PI)
 
 #endif /* MYMATH_HPP */
