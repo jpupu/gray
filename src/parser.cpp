acea96b9f419cce55a78077e71c9a25100fd2a6f
jpupu <arthamster@gmail.com>
2013-07-16 18:10:08 +0300

    some clean up

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,37 +11,97 @@ extern "C" {
 
 using std::string;
 
-struct Rule
+
+class Scanner
 {
-	Rule (const std::string& type, const std::string& pat)
-		: type(type),
-		pattern("^"+pat)
+public:
+	struct Rule
 	{
-		const char* err;
-		re.reset(trex_compile(pattern.c_str(), &err), [](TRex* ptr){trex_free(ptr);});
-		if (re == nullptr) {
-			throw;
+		Rule (const std::string& type, const std::string& pat)
+			: type(type),
+			pattern("^"+pat)
+		{
+			const char* err;
+			re.reset(trex_compile(pattern.c_str(), &err), [](TRex* ptr){trex_free(ptr);});
+			if (re == nullptr) {
+				throw;
+			}
 		}
-	}
 
-	std::string type;
-	std::string pattern;
-	std::shared_ptr<TRex> re;
-};
+		std::string type;
+		std::string pattern;
+		std::shared_ptr<TRex> re;
+	};
 
-struct Token
-{
-	Token () {}
-	Token (const std::string& t, const std::string& v)
-		: type(t)
+	struct Token
 	{
-		v_name = v;
-		v_number = atof(v.c_str());
+		Token () {}
+		Token (const std::string& t, const std::string& v)
+			: type(t)
+		{
+			v_name = v;
+			v_number = atof(v.c_str());
+		}
+		std::string type;
+		float		v_number;
+		std::string	v_name;
+	};
+
+
+private:
+	std::string source;
+	const char* p;
+	std::vector<Rule> rules;
+	Token current;
+	bool backed;
+
+public:
+	Scanner (const std::string& source)
+		: source(source),
+		p(this->source.c_str()),
+		backed(false)
+	{
+		rules = {
+			Rule("lparen", "\\("),
+			Rule("rparen", "\\)"),
+			Rule("number", "-?(\\d*\\.)?\\d+"),
+			Rule("name", "\\w+")
+		};
 	}
-	std::string type;
-	float		v_number;
-	std::string	v_name;
+
+	const Token& next ()
+	{
+		// std::cout << "SCAN(" << backed << ")\n";
+		if (backed) {
+			backed = false;
+			return current;
+		}
+
+		while (*p == ' ' || *p == '\n' || *p == '\t' || *p == '\r') ++p;
+
+		const char* o0;
+		const char* o1;
+		for (const Rule& r : rules) {
+			if (trex_search(r.re.get(), p, &o0, &o1)) {
+				p = o1;
+				current = Token(r.type, std::string(o0, o1));
+				// std::cout << "SCAN " << std::string(o0, o1) << std::endl;
+				return current;
+			}
+		}
+
+		current =  Token("eot", "");
+		return current;
+	}
+
+	const Token& back ()
+	{
+		if (backed) throw;
+		backed = true;
+		return current;
+	}
 };
+
 
 class ListItem;
 typedef std::vector<ListItem> List;
@@ -69,61 +129,7 @@ struct ListItem
 };
 
 
-class Scanner
-{
-private:
-	std::string source;
-	const char* p;
-	std::vector<Rule> rules;
-	Token current;
-	bool backed;
 
-public:
-	Scanner (const std::string& source)
-		: source(source),
-		p(this->source.c_str()),
-		backed(false)
-	{
-		rules = {
-			Rule("lparen", "\\("),
-			Rule("rparen", "\\)"),
-			Rule("number", "-?(\\d*\\.)?\\d+"),
-			Rule("name", "\\w+")
-		};
-	}
-
-	const Token& next ()
-	{
-		std::cout << "SCAN(" << backed << ")\n";
-		if (backed) {
-			backed = false;
-			return current;
-		}
-
-		while (*p == ' ' || *p == '\n' || *p == '\t' || *p == '\r') ++p;
-
-		const char* o0;
-		const char* o1;
-		for (const Rule& r : rules) {
-			if (trex_search(r.re.get(), p, &o0, &o1)) {
-				p = o1;
-				current = Token(r.type, std::string(o0, o1));
-				std::cout << "SCAN " << std::string(o0, o1) << std::endl;
-				return current;
-			}
-		}
-
-		current =  Token("eot", "");
-		return current;
-	}
-
-	const Token& back ()
-	{
-		if (backed) throw;
-		backed = true;
-		return current;
-	}
-};
 
 void print_list (const List* l, bool newline=true)
 {
@@ -145,7 +151,7 @@ const List* get_list (Scanner& scanner)
 	}
 
 	List* res = new List();
-	Token tok;
+	Scanner::Token tok;
 	while ((tok = scanner.next()).type != "rparen") {
 		if (tok.type == "eot") throw;
 		else if (tok.type == "number") {
@@ -164,19 +170,12 @@ const List* get_list (Scanner& scanner)
 	return res;
 }
 
+
+
 int main ()
 {
-	std::vector<Rule> rules = {
-		Rule("lparen", "\\("),
-		Rule("rparen", "\\)"),
-		Rule("number", "-?(\\d*\\.)?\\d+"),
-		Rule("name", "\\w+")
-	};
-
-	std::vector<Token> tokens;
-
-	std::string s ("the subject of this 2.3 subsequence is -1 submarines");
 	std::ifstream ifs ("sample1.scene");
+	std::string s;
 	s.assign(std::istreambuf_iterator<char>(ifs),
 			 std::istreambuf_iterator<char>());
 
@@ -184,144 +183,6 @@ int main ()
 	const List* l = get_list(scanner);
 	print_list(l);
 
-	// const char* o0;
-	// const char* o1;
-	// const char* p = s.c_str();
-	// bool found = true;
-	// while (found) {
-	// 	found = false;
-
-	// 	while (*p == ' ' || *p == '\n' || *p == '\t' || *p == '\r') ++p;
-	// 	// while (((*p) == ' ') || ((*p) == '\n') || ((*p) == '\t') || ((*p) == '\r')) {std::cout<<"skip\n";++p;}
-
-	// 	for (const Rule& r : rules) {
-	// 		if (trex_search(r.re.get(), p, &o0, &o1)) {
-	// 			tokens.push_back(Token(r.type, std::string(o0, o1)));
-	// 			std::cout << r.type << ": [";
-	// 			for (const char* c = o0; c!=o1; ++c) std::cout << *c;
-	// 			std::cout << "]" << std::endl;
-	// 			p = o1;
-	// 			found = true;
-	// 			break;
-	// 		}
-	// 	}
-
-	// }
-
-	// std::cout << "----------------\n";
-	// List root;
-	// int nest = 0;
-	// for (const auto& t : tokens) {
-	// 	std::cout << t.type << std::endl;
-	// 	switch (t.type) {
-	// 		case Token::LPAREN:
-
-	// }
-
   	return 0;
 }
-
-// struct Token
-// {
-// 	enum Type { STRING, NUMBER, LET, LPAREN, RPAREN, NEWLINE};
-// 	Type	type;
-// 	float	v_float;
-// 	string	v_string;
-// };
-
-// class Lexer
-// {
-// 	// enum State {};
-// 	// State state;
-// 	vector<Token> tokens;
-// 	// string buf;
-
-// };
-
-
-// void Lexer::feed ()
-// {
-
-// }
-
-
-// void Lexer::feed (char s)
-// {
-// 	switch (state) {
-// 		case S_IDLE:
-// 			if (s == ' ' || s == '\t' || s == '\r' || s == '\n') next();
-// 			else if (s == '(') { push(Token(Token::LPAREN)); next(); }
-// 			else if (s == ')') { push(Token(Token::RPAREN)); next(); }
-// 			else if (s == '=') { push(Token(Token::LET)); next(); }
-// 			else if (s == '.' || s == '-' || (s >= '0' && s <= '9')) {
-// 				state = S_NUMBER_START;
-// 			}
-// 			else if ((s >= 'a' && s <= 'z') || s == '_') {
-// 				state = S_STRING_START;
-// 			}
-// 			;;;;
-// 			break;
-// 		case S_NUMBER_START:
-// 			if (s == '-' || (s >= '0' && s <= '9')) {
-// 				buf += s;
-// 				state = S_NUMBER;
-// 				next();
-// 			}
-// 			else if (s == '.') {
-// 				buf += s;
-// 				state = S_NUMBER_AFTER;
-// 				next();
-// 			}
-// 			else error("Expecting starting number", s);
-// 			break;
-// 		case S_NUMBER:
-// 			if (s >= '0' && s <= '9') {
-// 				buf += s;
-// 				next();
-// 			}
-// 			else if (s == '.') {
-// 				buf += s;
-// 				state = S_NUMBER_AFTER;
-// 				next();
-// 			}
-// 			else {
-// 				state = IDLE;
-// 				push(Token(Token::NUMBER, std::stof(buf)));
-// 				buf = "";
-// 				next();
-// 			}
-// 			break;
-// 		case S_NUMBER_AFTER:
-// 			if (s >= '0' && s <= '9') {
-// 				buf += s;
-// 				next();
-// 			}
-// 			else {
-// 				state = IDLE;
-// 				push(Token(Token::NUMBER, std::stof(buf)));
-// 				buf = "";
-// 			}
-// 			break;
-// 		case S_STRING_START:
-// 			if ((s >= 'a' && s <= 'z') || s == '_') {
-// 				buf += s;
-// 				state = S_STRING;
-// 				next();
-// 			}
-// 			break;
-// 		case S_STRING:
-// 			if ((s >= 'a' && s <= 'z') || s == '_' || (s >= '0' && s <= '9')) {
-// 				buf += s;
-// 				next();
-// 			}
-// 			else {
-// 				state = IDLE;
-// 				push(Token(Token::STRING, buf));
-// 				buf = "";
-// 			}
-// 			break;
-// 	}
-// }
-
-
 
