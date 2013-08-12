9c90c965dec91f1db38003cf0d55064ae99be93e
jpupu <arthamster@gmail.com>
2013-08-12 21:56:53 +0300

    separated stuff into gray.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "Transform.hpp"
+
 #include "film.hpp"
 
 float frand ()
@@ -63,36 +63,7 @@ vec3 sample_hemisphere (const vec2& uv)
 }
 
 
-struct Ray
-{
-    static constexpr float epsilon = 1e-6f;
-
-    vec3 o;
-    vec3 d;
-    float tmin, tmax;
-
-    Ray (const vec3& o, const vec3& d,
-         float tmin = epsilon, float tmax = INFINITY)
-        : o(o), d(d), tmin(tmin), tmax(tmax)
-    { }
-
-    Ray transform (const Transform& xform) const
-    {
-        return Ray(xform.point(o), xform.vector(d), tmin, tmax);
-    }
-};
-
-class Material;
-
-struct Isect
-{
-    vec3 p;
-    vec3 n;
-    Material* mat;
-};
-
-
-class Sphere
+class Sphere : public Shape
 {
 public:
     bool intersect (Ray& r, Isect* isect);
@@ -136,16 +107,6 @@ public:
     Spectrum R;
 };
 
-class BSDF
-{
-public:
-    virtual ~BSDF () {}
-
-    /// @param wo [in]  exiting vector in tangent space, normalized
-    /// @param wi [out] entering vector in tangent space, normalized
-    /// @return reflectance f(wo,wi)
-    virtual Spectrum sample (const vec3& wo, vec3* wi, const vec2& uv) const = 0;
-};
 
 class Lambertian : public BSDF
 {
@@ -160,69 +121,11 @@ public:
     }
 };
 
-
-class Material
+BSDF* Material::get_bsdf (const vec3& p) const
 {
-public:
-    Spectrum R;
+    return new Lambertian(R);
+}
 
-    BSDF* get_bsdf (const vec3& p) const
-    {
-        return new Lambertian(R);
-    }
-};
-
-class Primitive
-{
-public:
-    virtual bool intersect (Ray& r, Isect* isect) const = 0;
-};
-
-
-class ListAggregate : public Primitive
-{
-public:
-    std::vector<const Primitive*> prims;
-
-    void add (const Primitive* p)
-    {
-        prims.push_back(p);
-    }
-
-    bool intersect (Ray& r, Isect* isect) const
-    {
-        bool hit = false;
-        for (const Primitive* prim : prims) {
-            hit |= prim->intersect(r, isect);
-        }
-        return hit;
-    }
-};
-
-class GeometricPrimitive : public Primitive
-{
-public:
-    Material* mat;
-    Sphere* shape;
-    Transform world_from_prim;
-    mutable Transform prim_from_world;
-
-    bool intersect (Ray& r, Isect* isect) const
-    {
-        prim_from_world = inverse(world_from_prim); // FIXME: dont do this every time!
-
-        Ray ro = r.transform(prim_from_world);
-        if (!shape->intersect(ro, isect)) {
-            return false;
-        }
-
-        r.tmax = ro.tmax;
-        isect->p = world_from_prim.point(isect->p);
-        isect->n = world_from_prim.normal(isect->n);
-        isect->mat = mat;
-        return true;
-    }
-};
 
 
 
@@ -235,14 +138,14 @@ int main (int argc, char* argv[])
 
     Material mat1 = { Spectrum(1.0f, 0.0f, .5f) };
     Material mat2 = { Spectrum(.8f, 1.0f, .5f) };
-    Sphere sp1;
+    Shape* sp1 = new Sphere();
     GeometricPrimitive prim1;
     prim1.mat = &mat1;
-    prim1.shape = &sp1;
+    prim1.shape = sp1;
     prim1.world_from_prim = Transform::rotate(90, vec3(0,1,0));
     GeometricPrimitive prim2;
     prim2.mat = &mat2;
-    prim2.shape = &sp1;
+    prim2.shape = sp1;
     prim2.world_from_prim = Transform::translate(vec3(1.4,1,-1));
     ListAggregate list;
     list.add(&prim1);
